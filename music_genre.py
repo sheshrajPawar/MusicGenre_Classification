08ac0487a9650b6f1f97153985545445464cb53f
Sheshraj Pawar <143826741+sheshrajPawar@users.noreply.github.com>
2024-09-23 16:24:14 +0530

    Create music_genre.py

---
+++ music_genre.py
@@ -0,0 +1,12 @@
+from python_speech_features import mfcc
+import scipy.io.wavfile as wav
+import numpy as np
+
+from tempfile import TemporaryFile
+import os
+import pickle
+import random 
+import operator
+
+import math
+import numpy as np
